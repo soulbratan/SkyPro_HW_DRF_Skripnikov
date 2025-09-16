2746391850e1a41ae5fe5008b301180d595cfa0b
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-09-16 17:43:49 +0700

    create models: User, Course, Lesson

---
+++ users/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Введите email"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите телефон",
+    )
+    country = models.CharField(
+        max_length=50,
+        verbose_name="Страна",
+        blank=True,
+        null=True,
+        help_text="Введите страну",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Добавьте аватар",
+    )
+    token = models.CharField(
+        max_length=100, verbose_name="Token", blank=True, null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
