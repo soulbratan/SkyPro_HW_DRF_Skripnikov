2a849436f3f1a5f5b768e92e7e9d8f1785843d9b
Oleg Skripnikov <oleg-skripnikov8@mail.ru>
2025-09-18 18:45:16 +0700

    add settings for JWT django

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "users",
     "lms",
     "django_filters",
+    "rest_framework_simplejwt",
 ]
 
 
@@ -71,6 +72,15 @@ TEMPLATES = [
 
 
 WSGI_APPLICATION = "config.wsgi.application"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+
+}
 
 
 DATABASES = {
